97164ff550c2c9b215e587c17370a35390580c9e
udk-wtag <kaium.uddin@welldev.io>
2024-08-11 14:37:19 +0600

    use shoulda matcher

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,42 +1,40 @@
+require 'rails_helper'
+
 RSpec.describe SessionsController, type: :controller do
-  let!(:user) { create(:user) }
+  let!(:user) { FactoryBot.create(:user) }
 
-  describe "GET #new" do
-    it "returns a successful response" do
-      get :new
-      expect(response).to have_http_status(:success)
+  describe 'GET #new' do
+    before { get :new }
+
+    it { is_expected.to respond_with(:success) }
+    it { is_expected.to render_template(:new) }
+  end
+
+  describe 'POST #create' do
+    context 'with valid credentials' do
+      before { post :create, params: { email: user.email, password: user.password } }
+
+      it { is_expected.to set_session[:user_id].to(user.id) }
+      it { is_expected.to redirect_to(user_path(user)) }
+      it { is_expected.to set_flash[:notice].to("Welcome back, #{user.name}!") }
+    end
+
+    context 'with invalid credentials' do
+      before { post :create, params: { email: user.email, password: 'wrongpassword' } }
+
+      it { is_expected.not_to set_session[:user_id] }
+      it { is_expected.to render_template(:new) }
+      it { is_expected.to set_flash.now[:alert].to('Invalid email or password') }
     end
   end
 
-  describe "POST #create" do
-    context "with valid credentials" do
-      it "logs in the user and redirects to the user's show page" do
-        post :create, params: { email: user.email, password: user.password }
-        expect(session[:user_id]).to eq(user.id)
-        expect(response).to redirect_to(user_path(user))
-        expect(flash[:notice]).to eq("Welcome back, #{user.name}!")
-      end
-    end
-
-    context "with invalid credentials" do
-      it "does not log in the user and re-renders the new template with an alert" do
-        post :create, params: { email: user.email, password: "wrongpassword" }
-        expect(session[:user_id]).to be_nil
-        expect(response).to render_template(:new)
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(flash.now[:alert]).to eq("Invalid email or password")
-      end
-    end
-  end
-
-  describe "DELETE #destroy" do
-    it "logs out the user and redirects to the root URL with a notice" do
+  describe 'DELETE #destroy' do
+    before do
       session[:user_id] = user.id
       delete :destroy
-      expect(session[:user_id]).to be_nil
-      expect(response).to redirect_to(root_url)
-      expect(response).to have_http_status(:see_other)
-      expect(flash[:notice]).to eq("You've been sign out!")
     end
+
+    it { is_expected.to set_session[:user_id].to(nil) }
+    it { is_expected.to redirect_to(root_url) }
   end
 end
