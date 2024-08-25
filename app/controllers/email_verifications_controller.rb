9e3455eb13c278eb0aab457c0bb1c2f780b6f70f
udk-wtag <kaium.uddin@welldev.io>
2024-08-25 15:24:59 +0600

    email verification done

--- app/controllers/email_verifications_controller.rb
+++ app/controllers/email_verifications_controller.rb
@@ -3,9 +3,13 @@ class EmailVerificationsController < ApplicationController
     user = User.find_by(verification_token: params[:token])
 
     if user && !user.email_verified
-      user.update(email_verified: true, verification_token: nil)
-      flash[:notice] = "Your email has been successfully verified."
-      redirect_to new_session_path
+      if user.update(email_verified: true, verification_token: nil)
+        flash[:notice] = "Your email has been successfully verified."
+        redirect_to new_session_path
+      else
+        flash[:alert] = "Something went wrong. Please try again."
+        redirect_to root_path
+      end
     else
       flash[:alert] = "Invalid or expired verification link."
       redirect_to root_path
