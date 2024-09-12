8b1dc35d09a47507561a8ded59a3c4e5b9e0c1e9
Kaium Uddin <kaium.uddin@welldev.io>
2024-09-12 14:09:28 +0600

    add i18n on admin categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -20,8 +20,9 @@ module Admin
     def create
       @category = Category.new(category_params)
       if @category.save
-        redirect_to admin_category_path(@category), notice: 'Category was successfully created.'
+        redirect_to admin_category_path(@category), notice: t('admin.categories.messages.created')
       else
+        flash[:alert] = t('admin.categories.messages.save_error')
         render :new, status: :unprocessable_entity
       end
     end
@@ -32,15 +33,16 @@ module Admin
 
     def update
       if @category.update(category_params)
-        redirect_to admin_category_path(@category), notice: 'Category was successfully updated.'
+        redirect_to admin_category_path(@category), notice: t('admin.categories.messages.updated')
       else
+        flash[:alert] = t('admin.categories.messages.save_error')
         render :edit, status: :unprocessable_entity
       end
     end
 
     def destroy
       @category.destroy
-      redirect_to admin_categories_path, notice: 'Category was successfully deleted.', status: :see_other
+      redirect_to admin_categories_path, notice: t('admin.categories.messages.deleted'), status: :see_other
     end
 
     private
@@ -54,11 +56,11 @@ module Admin
     end
 
     def require_admin
-      redirect_to root_path, alert: "Access Denied!" unless current_user&.admin?
+      redirect_to root_path, alert: t('admin.shared.access_denied') unless current_user&.admin?
     end
 
     def require_signin
-      redirect_to new_session_path, alert: "Please sign in first!" unless current_user
+      redirect_to new_session_path, alert: t('admin.shared.please_sign_in') unless current_user
     end
   end
 end
