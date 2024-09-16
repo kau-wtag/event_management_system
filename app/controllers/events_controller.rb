13f56680191137304283022647dbbaff30fabfc8
Kaium Uddin <kaium.uddin@welldev.io>
2024-09-16 10:42:46 +0600

    events filtering

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,12 +3,38 @@ class EventsController < ApplicationController
   before_action :require_admin, except: [:index, :show]
 
   def index
+    @events = Event.all
+
+    # Search filtering
     if params[:search].present?
-      @events = Event.search_by_name_and_description(params[:search]).page(params[:page]).per(10)
-    else
-      @events = Event.all.page(params[:page]).per(2)
+      @events = @events.search_by_name_and_description(params[:search])
     end
+
+    # Category filtering
+    if params[:category].present?
+      @events = @events.where(category_id: params[:category])
+    end
+
+    # Location filtering
+    if params[:location].present?
+      @events = @events.where('location ILIKE ?', "%#{params[:location]}%")
+    end
+
+    # Price range filtering
+    if params[:min_price].present? && params[:max_price].present?
+      @events = @events.where(price: params[:min_price]..params[:max_price])
+    end
+
+    # Date range filtering
+    if params[:start_date].present? && params[:end_date].present?
+      @events = @events.where(starts_at: params[:start_date]..params[:end_date])
+    end
+
+    # Pagination with per page selection
+    per_page = params[:per_page] || 10
+    @events = @events.page(params[:page]).per(per_page)
   end
+
 
   def new
     @event = Event.new
