87ac5ec54e376b1eaf2ca305e77984437056bf7f
Kaium Uddin <kaium.uddin@welldev.io>
2024-09-18 10:01:57 +0600

    total upcoming past

--- app/controllers/organizer/dashboard_controller.rb
+++ app/controllers/organizer/dashboard_controller.rb
@@ -7,6 +7,15 @@ module Organizer
     def index
       # Get all events created by the current organizer
       @events = current_user.events
+      
+      # Total number of events
+      @total_events = @events.count
+
+      # Calculate the number of upcoming events
+      @upcoming_events = @events.where("starts_at > ?", Time.zone.now).count
+
+      # Calculate the number of past events
+      @past_events = @events.where("starts_at < ?", Time.zone.now).count
     end
   end
 end
