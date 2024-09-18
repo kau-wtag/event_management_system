9b8d5563ae3b7e70956b9bae9351202d4728d0a9
Kaium Uddin <kaium.uddin@welldev.io>
2024-09-18 10:11:20 +0600

    event performance

--- app/controllers/organizer/dashboard_controller.rb
+++ app/controllers/organizer/dashboard_controller.rb
@@ -7,7 +7,7 @@ module Organizer
     def index
       # Get all events created by the current organizer
       @events = current_user.events
-      
+
       # Total number of events
       @total_events = @events.count
 
@@ -16,6 +16,20 @@ module Organizer
 
       # Calculate the number of past events
       @past_events = @events.where("starts_at < ?", Time.zone.now).count
+
+      # Top performing event based on the highest number of registrations
+      @top_performing_event = @events.max_by { |event| event.registrations.count }
+
+      # Least performing event based on the lowest number of registrations
+      @least_performing_event = @events.min_by { |event| event.registrations.count }
+
+      # Average registrations per event
+      total_registrations = @events.sum { |event| event.registrations.count }
+      @average_registrations_per_event = total_registrations.to_f / @events.count if @events.count > 0
+
+      # Average revenue per event (calculated as event price * number of registrations)
+      total_revenue = @events.sum { |event| event.price * event.registrations.count }
+      @average_revenue_per_event = total_revenue.to_f / @events.count if @events.count > 0
     end
   end
 end
