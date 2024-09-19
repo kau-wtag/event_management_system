5071e0cdd76cdd86534319b1cdad94beea5d80b5
Kaium Uddin <kaium.uddin@welldev.io>
2024-09-19 14:39:22 +0600

    admin category

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
       get 'dashboard/index'
       resources :events, only:[:index, :show]
       resources :users, only: [:index, :show, :destroy]
-      resources :categories
+      resources :categories, only: [:new, :index, :edit, :create, :destroy]
     end
 
     # Organizer namespace for organizer-specific routes
