6adb020afa7e506dba4cb199d52c945182f4e269
Kaium Uddin <kaium.uddin@welldev.io>
2024-09-22 18:20:50 +0600

    users events updated

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,5 @@
 class EventsController < ApplicationController
   before_action :require_signin, except: [:index, :show]
-  before_action :require_admin, except: [:index, :show]
 
   def index
     @events = Event.all
@@ -35,48 +34,7 @@ class EventsController < ApplicationController
     @events = @events.page(params[:page]).per(per_page)
   end
 
-
-  def new
-    @event = Event.new
-  end
-
-  def create
-    @event = Event.new(event_params)
-    if @event.save
-      redirect_to event_path(@event), notice: t('events.messages.created')
-    else
-      flash.now[:alert] = t('events.messages.error')
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
   def show
     @event = Event.find(params[:id])
-  end
-
-  def edit
-    @event = Event.find(params[:id])
-  end
-
-  def update
-    @event = Event.find(params[:id])
-    if @event.update(event_params)
-      redirect_to event_path(@event), notice: t('events.messages.updated')
-    else
-      flash.now[:alert] = t('events.messages.error')
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @event = Event.find(params[:id])
-    @event.destroy
-    redirect_to events_url, notice: t('events.messages.deleted'), status: :see_other
-  end
-
-  private
-
-  def event_params
-    params.require(:event).permit(:name, :description, :location, :price, :starts_at, :capacity, :image)
   end
 end
